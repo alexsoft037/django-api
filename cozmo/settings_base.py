38379e006af2e214523c55010ea4301950ed2db9
alex s <alexsoft037@outlook.com>
2022-02-17 10:56:40 -0800

    make it better

--- cozmo/settings_base.py
+++ cozmo/settings_base.py
@@ -19,6 +19,7 @@ from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 def _required_env(name):
